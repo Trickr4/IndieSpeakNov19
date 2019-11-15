f680881182511c16fcc3907905c22a3d9180c103
Jonathan Chhou <chhouj@uci.edu>
2019-11-14 21:52:58 -0800

    Added location selection script.

---
+++ Assets/Scripts/LocationSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LocationSelection : MonoBehaviour, IPointerClickHandler
+{
+    Canvas template;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        template = gameObject.GetComponentInParent(typeof(Canvas)) as Canvas;
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Debug.Log(name + " Game Object Clicked!");
+        template.gameObject.SetActive(false);
+    }
+}
